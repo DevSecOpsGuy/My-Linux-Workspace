ec5f2d0a59a400338b69a2919a7f1f5be7b36c56
Saumen <saumenroy323@gmail.com>
2019-06-30 18:19:33 +0600

    Add files via upload

---
+++ My setup.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt dist-upgrade
+#PPA
+sudo add-apt-repository ppa:papirus/papirus
+sudo apt-add-repository ppa:tista/adapta
+sudo add-apt-repository ppa:damentz/liquorix
+sudo apt-get update
+
+#In the main repo
+sudo apt install vlc smplayer audacious chromium-browser qbittorrent git neofetch
